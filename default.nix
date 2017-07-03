b960d0279702582946a1bfa8cfd2fdfa3cb52e81
Piotr Bogdan <ppbogdan@gmail.com>
2017-07-03 21:58:35 +0100

    Pin nixpkgs but allow overwrite.

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
-{ compiler ? "ghc802" }:
-let nixpkgs = import ((import <nixpkgs> {}).fetchFromGitHub {
+{ pkgs ? null, compiler ? "ghc802" }:
+let nixpkgs = if pkgs == null then
+              import ((import <nixpkgs> {}).fetchFromGitHub {
                 owner = "NixOS";
                 repo = "nixpkgs";
                 rev = "2e983f14f6";
                 sha256 = "0rm5kb8l27kb2bac95zqc18v50065iix83sv3l0v9wpkq7421cby";
-             }) {};
-
+             }) {}
+             else
+             import <nixpkgs> {};
 in
 nixpkgs
  .pkgs
