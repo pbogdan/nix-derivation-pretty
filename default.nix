0a0b26305e678b88f9d14999b5127f4c71bdbc2f
Piotr Bogdan <ppbogdan@gmail.com>
2017-07-03 21:10:14 +0100

    Pin nixpkgs.

--- default.nix
+++ default.nix
@@ -1,4 +1,12 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }:
+{ compiler ? "ghc802" }:
+let nixpkgs = import ((import <nixpkgs> {}).fetchFromGitHub {
+                owner = "NixOS";
+                repo = "nixpkgs";
+                rev = "2e983f14f6";
+                sha256 = "0rm5kb8l27kb2bac95zqc18v50065iix83sv3l0v9wpkq7421cby";
+             }) {};
+
+in
 nixpkgs
  .pkgs
  .haskell
