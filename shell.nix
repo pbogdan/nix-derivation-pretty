e8230ea1c68dfbe084a06bc9692b18853050d917
Piotr Bogdan <ppbogdan@gmail.com>
2017-09-14 15:46:32 +0100

    Fix nix-shell.

--- shell.nix
+++ shell.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+{ pkgs ? null, compiler ? "ghc802" }:
+(import ./default.nix { inherit pkgs compiler; }).env
