2b850062ad4a22d38a124f1ebb3438095c2a0f83
Piotr Bogdan <ppbogdan@gmail.com>
2017-11-02 15:10:38 +0000

    Get rid of pinning for now, increase & fix test coverage.

--- default.nix
+++ default.nix
@@ -1,17 +1,7 @@
-{ pkgs ? null, compiler ? "ghc802" }:
-let nixpkgs = if pkgs == null then
-              import ((import <nixpkgs> {}).fetchFromGitHub {
-                owner = "NixOS";
-                repo = "nixpkgs";
-                rev = "2e983f14f6";
-                sha256 = "0rm5kb8l27kb2bac95zqc18v50065iix83sv3l0v9wpkq7421cby";
-             }) {}
-             else
-             import <nixpkgs> {};
-in
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc802" }:
 nixpkgs
  .pkgs
  .haskell
  .packages
  .${compiler}
- .callPackage ./nix-derivation-pretty.nix rec { }
+ .callPackage ./nix-derivation-pretty.nix { }
